0269af5f3d68dd3a58e44e98762a270144b6bda4
HUBERTSUNIL <sunilhubert18@gmail.com>
2023-11-08 08:51:40 +0530

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,40 @@
 import React from 'react'
 
 const Form = () => {
-    
+
     return (
-    <div class="form-container">
-        <form>
-            <div class="input-container">
-                <label for="rollNumber">Roll Number</label>
-                <input type="text" id="rollNumber" name="rollNumber" required/>
-            </div>
+        <div class="form-container">
+            <form>
+                <div class="input-container">
+                    <div class="input-container-inside">
+                        <label for="rollNumber">Roll Number</label>
+                        <input className='input-container-input' type="text" id="rollNumber" name="rollNumber" required />
+                    </div>
+                    <div class="input-container-inside">
+                        <label for="name">Name</label>
+                        <input className='input-container-input'  type="text" id="name" name="name" required />
+                    </div>
+                </div>
 
-            <div class="input-container">
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name" required/>
-            </div>
+                <div class="input-container">
+                    <div class="input-container-inside">
+                        <label for="disease">Disease</label>
+                        <input className='input-container-input' type="text" id="disease" name="disease" required />
+                    </div>
+                    <div class="input-container-inside">
+                        <label for="name">Symptoms</label>
+                        <input className='input-container-input' type="text" id="symptom" name="symptom" required />
+                    </div>
+                </div>
 
-            <div class="input-container">
-                <label for="disease">Disease</label>
-                <input type="text" id="disease" name="disease" required/>
-            </div>
+                <div class="input-container-note">
+                    <label for="note">Note</label>
+                    <textarea id="note" name="note" rows="4"></textarea>
+                </div>
 
-            <div class="input-container">
-                <label for="note">Note</label>
-                <textarea id="note" name="note" rows="4"></textarea>
-            </div>
-
-            <button type="submit" class="add-button" onClick={onclick}>Add</button>
-        </form>
-    </div>
+                <button type="submit" class="add-button" onClick={onclick}>Add</button>
+            </form>
+        </div>
     )
 }
 
--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
+import Chart from "react-apexcharts";
+import Form from './Form';
+
 
 const Statistic = () => {
+  const [options] = useState({
+    chart: {
+      id: "basic-bar"
+    },
+    xaxis: {
+      categories: ['Fever', 'Headache', 'Cough', 'Runny Nose', 'Cold', 'Diarrhea', 'Asthma', 'Pneumonia', 'Acidity', 'Conjunctivitis']
+    }
+  });
+
+  const [series] = useState([
+    {
+      name: "Patients",
+      data: [10, 7, 15, 12, 13, 4, 2, 1, 5, 3]
+    }
+  ]);
+  const [time] = useState({
+    chart: {
+      id: "Graph"
+    },
+    xaxis: {
+      categories: ['9:00','9:30','10:00','10:30','11:00','11:30','12:00','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30']
+    }
+  });
+  const [rush] = useState([
+    {
+      name: "Patients",
+      data: [10, 7, 15, 12, 13, 4, 2, 1, 5, 3]
+    }
+  ]);
+
   return (
-    <div>
-      <Navbar/> 
+    <div className="app">
+      <Navbar />
+      <div className="d-flex justify-content-evenly my-3 p-3">
+        <div className="mixed-chart my-3 p-3" id='chart1'>
+          <Chart options={options} series={series} type="bar" width={500} />
+        </div>
+        <div className='my-3 p-3' id="chart2">
+          <Chart
+            options={time}
+            series={rush}
+            type="line"
+            width="500"
+          />
+        </div>
+
+      </div>
+      <Form/>
     </div>
   )
-}
-
+};
 export default Statistic
